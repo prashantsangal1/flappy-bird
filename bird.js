68a072978da6cac40163bb5a39cafe9bbe21f7a2
prashantsangal1 <69078671+prashantsangal1@users.noreply.github.com>
2020-12-12 22:27:28 +0530

    Update bird.js

--- bird.js
+++ bird.js
@@ -1,7 +1,7 @@
 class Bird{
     constructor(){
         this.body = Bodies.rectangle(50,175,10,10);
-        this.image = loadImage("images/flappybird.gif");
+        this.image = loadImage("flappybird.gif");
     }
     display(){
         image(this.image,this.body.position.x,this.body.position.y,50,50);
